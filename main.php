c39e27d9639943056c10cb274752263a18aa140b
William Smith <wsmith1@mail.ru>
2011-06-04 22:47:20 -0400

    Update template to Dokuwiki version 2011-05-25

--- main.php
+++ main.php
@@ -29,7 +29,7 @@ if (!defined('DOKU_INC')) die();
 
   <?php tpl_metaheaders()?>
 
-  <link rel="shortcut icon" href="<?php echo DOKU_TPL?>images/favicon.ico" />
+  <link rel="shortcut icon" href="<?php echo tpl_getFavicon() ?>" />
 
   <?php /*old includehook*/ @include(dirname(__FILE__).'/meta.html')?>
 </head>
@@ -133,7 +133,7 @@ if (!defined('DOKU_INC')) die();
 	
   </div>
 
-  <div class="clearer">&nbsp;</div>
+  <div class="clearer"></div>
 
 
 
